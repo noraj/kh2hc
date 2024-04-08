d26dfaa80f0f8fcd0ee343f9749ce44ba2fa4e61
Alexandre ZANNI <alexandre.zanni@europe.com>
2024-04-08 18:44:37 +0200

    Lint: Style/MapIntoArray

--- lib/kh2hc.rb
+++ lib/kh2hc.rb
@@ -13,23 +13,20 @@ module Kh2hc
   # @return [Array<Hash>] An array of Hash. Each Hash has two keys: the `:hash` of the host hash,
   #   the `:salt` of the host hash
   def self.convert(khfile)
-    hosts = []
     data = File.read(khfile)
     # |<Magic string>|<salt>|<hash> <key algorithm> <public key sig.>
-    data.scan(/^\|1\|([^|]+)\|([^|].+) .+ .+$/).each do |host|
+    data.scan(/^\|1\|([^|]+)\|([^|].+) .+ .+$/).map do |host|
       # hash:salt
-      hosts << { hash: host[1].from_b64.to_hex, salt: host[0].from_b64.to_hex }
+      { hash: host[1].from_b64.to_hex, salt: host[0].from_b64.to_hex }
     end
-    hosts
   end
 
   # Convert OpenSSH known_hosts file hashed with HashKnownHosts to a hash file crackable by Hashcat.
   # @param khfile [String] OpenSSH known_hosts file
   # @return [String] hash file in Hashcat format
   def self.convert1(khfile)
-    hc_out = []
-    convert(khfile).each do |host|
-      hc_out << "#{host[:hash]}:#{host[:salt]}"
+    hc_out = convert(khfile).map do |host|
+      "#{host[:hash]}:#{host[:salt]}"
     end
     hc_out.join("\n")
   end
